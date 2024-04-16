98da152f5c778a03a87a23ea6b6b6df0d9e212ae
nachosk1 <104322849+nachosk1@users.noreply.github.com>
2024-04-16 19:09:30 -0400

    Update SectionIcons.jsx

--- app/components/home/SectionIcons.jsx
+++ app/components/home/SectionIcons.jsx
@@ -5,21 +5,21 @@ export default function SectionIcons() {
         <div className="grid grid-cols-3 gap-y-4 place-items-center">
             <div>
                 <Picture
-                    url={'/home/logo-accredited.jpg'}
+                    url={'/home/logo-accredited.webp'}
                     alt="Logo acreditado"
                     style="w-24 md:w-40"
                 />
             </div>
             <div>
                 <Picture
-                    url={'/home/logo-muni.png'}
+                    url={'/home/logo-muni.webp'}
                     alt="Logo acreditado"
                     style="w-24 md:w-40"
                 />
             </div>
             <div>
                 <Picture
-                    url={'/home/logo-centro-de-salud.jpg'}
+                    url={'/home/logo-centro-de-salud.webp'}
                     alt="Logo acreditado"
                     style="w-24 md:w-40"
                 />
