6900ed63086fe00caba46d92a0567cd22441620c
nachosk1 <104322849+nachosk1@users.noreply.github.com>
2024-04-15 21:25:19 -0400

    Update Navbar y Footer
    Se cambiaron estilos y agregaron imagenes para continuacion

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -9,18 +9,20 @@ export default function Footer() {
     return (
         <footer className="bg-white text-black border-t relative z-30">
             <Container>
-                <div className="flex flex-col xl:flex-row justify-between py-10 gap-8 xl:gap-24 px-4">
-                    <div className="flex flex-col justify-start items-start gap-6">
-                        <Logo type="large" />
-                        <div className="hidden xl:block w-full">
-                            <Social />
+                <div className='mb-10'>
+                    <div className="flex flex-col xl:flex-row justify-between py-10 gap-8 xl:gap-24 px-4">
+                        <div className="flex flex-col justify-start items-start gap-6">
+                            <Logo type="large" />
+                            <div className="hidden xl:block w-full">
+                                <Social />
+                            </div>
                         </div>
+                        <ListFooter footerLinks={footerLinks} />
                     </div>
-                    <ListFooter footerLinks={footerLinks} />
-                </div>
 
-                <div className="xl:hidden h-4">
-                    <Social />
+                    <div className="xl:hidden h-4">
+                        <Social />
+                    </div>
                 </div>
             </Container>
 
--- app/components/navigation/Navbar.jsx
+++ app/components/navigation/Navbar.jsx
@@ -1,8 +1,8 @@
-import { navLinks } from "../../../constant";
-import Container from "../utils/Container";
-import Header from "./Header";
-import NavLink from "./NavLink";
-import SectionButtons from "./SectionButtons";
+import { navLinks } from '../../../constant';
+import Container from '../utils/Container';
+import Header from './Header';
+import NavLink from './NavLink';
+import SectionButtons from './SectionButtons';
 
 export default function Navbar() {
     return (
@@ -12,13 +12,18 @@ export default function Navbar() {
             </Container>
             <div className="bg-third py-4">
                 <Container>
-                    <div className="flex justify-between items-center">
-                        <ul className="space-x-10 md:space-x-0 text-gray-200 hidden md:grid md:grid-cols-3 md:gap-y-4 lg:grid-cols-4 lg:space-x-4 xl:flex xl:gap-x-8 xl:space-x-0 font-semibold">
-                            {navLinks.map(link => (
-                                <NavLink key={link.title} link={link} />
-                            ))}
-                        </ul>
-                        <SectionButtons />
+                    <div className="flex items-center md:justify-between">
+                        <div className="hidden md:block">
+                            <ul className=" text-gray-200 hidden md:grid md:grid-cols-3 md:gap-y-4 lg:grid-cols-4 lg:space-x-4 xl:flex xl:gap-x-8 xl:space-x-0 font-semibold flex-1">
+                                {navLinks.map(link => (
+                                    <NavLink key={link.title} link={link} />
+                                ))}
+                            </ul>
+                        </div>
+
+                        <div className="flex items-center gap-4">
+                            <SectionButtons />
+                        </div>
                     </div>
                 </Container>
             </div>
