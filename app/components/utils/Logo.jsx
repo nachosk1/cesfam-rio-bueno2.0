4553be158407772a4ebcc125bc0e21d929d6e391
nachosk1 <104322849+nachosk1@users.noreply.github.com>
2024-04-20 11:27:31 -0400

    details

--- app/components/utils/Logo.jsx
+++ app/components/utils/Logo.jsx
@@ -4,18 +4,18 @@ export default function Logo({ type = 'short' }) {
     const isLarge = type === 'large';
 
     return (
-        <div>
+        <div className='w1'>
             {isLarge ? (
                 <Picture
                     url={'/home/logo.png'}
                     alt={'Large Logo'}
-                    style={'aspect-square object-contain w-40 h-20'}
+                    style={'aspect-square object-contain w-28 h-12 md:w-40 md:h-20'}
                 />
             ) : (
                 <Picture
                     url={'/home/logo.png'}
                     alt={'Small Logo'}
-                    style={'aspect-square object-contain w-28 h-14'}
+                    style={'aspect-square object-contain w-20 h-10 md:w-28 md:h-14'}
                 />
             )}
         </div>
