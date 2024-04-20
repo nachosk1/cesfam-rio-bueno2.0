5d47d129cd5765073f724593c780cd62ee7d0200
nachosk1 <104322849+nachosk1@users.noreply.github.com>
2024-04-20 11:23:24 -0400

    Create enlaces-a-servicios-de-emergencias.jsx

---
+++ app/routes/enlaces-a-servicios-de-emergencias.jsx
@@ -0,0 +1,9 @@
+import Developing from '../components/utils/Developing';
+
+export default function ServiceEmergency() {
+    return (
+        <div className="mt-8 md:mt-10">
+            <Developing />
+        </div>
+    );
+}
